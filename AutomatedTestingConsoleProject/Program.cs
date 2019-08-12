644045cd91e4f681d6d6d30449867aa198dc2453
Greg Doud <gpdoud@gmail.com>
2019-08-12 10:50:19 -0400

    Add project files.

---
+++ AutomatedTestingConsoleProject/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AutomatedTestingConsoleProject {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AutomatedTestingLibraryProject/Functions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomatedTestingLibraryProject {
+
+    public class Functions {
+
+        public int Add1(int i) {
+            return i + 1;
+        }
+    }
+}
